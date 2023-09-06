6d92ebdb0f9fd01c498af37c494469c0d415ae38
praveenyadahalli <praveenya923@gmail.com>
2023-09-06 23:28:01 +0530

    Added Redirection on Successful Authentication.

--- src/main/java/com/webapp/web/controller/CustomerController.java
+++ src/main/java/com/webapp/web/controller/CustomerController.java
@@ -4,8 +4,10 @@ import com.webapp.web.entity.CustomerEntity;
 import com.webapp.web.entity.UserIdPasswordEntity;
 import com.webapp.web.response.CustomerResponse;
 import com.webapp.web.service.CustomerService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class CustomerController {
     private CustomerService customerService;
+    private String authorizationHeader="dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM=";
 
     @Autowired
     public CustomerController(CustomerService customerService) {
@@ -24,12 +27,15 @@ public class CustomerController {
 
     @PostMapping("/auth")
     public ResponseEntity<?> authenticateUser(@RequestParam("login_id") String loginId,
-                                              @RequestParam("password") String password) {
+                                              @RequestParam("password") String password, HttpServletRequest request) {
         try {
 //            String accessToken = customerService.authenticateUser(loginId, password);
             String accessToken="dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM=";
+
             if (accessToken != null) {
-                return ResponseEntity.ok("Authentication successful. Access Token: " + accessToken);
+                return ResponseEntity.status(HttpStatus.FOUND)
+                        .header("Location", request.getContextPath() + "/get-customers")
+                        .body(null);
             } else {
                 return ResponseEntity.badRequest().body("Authentication failed.");
             }
@@ -43,7 +49,7 @@ public class CustomerController {
         return ResponseEntity.ok(customerList);
     }
     @PostMapping("/create-customer")
-    public ResponseEntity<String> createCustomer(@RequestHeader("Authorization") String authorizationHeader, @RequestBody CustomerEntity customer) {
+    public ResponseEntity<String> createCustomer(@RequestHeader(name = "Authorization", defaultValue = "dGVzdEBzdW5iYXNlZGF0YS5jb206VGVzdEAxMjM=") String authorizationHeader, @RequestBody CustomerEntity customer) {
         String result = customerService.createCustomer(authorizationHeader, customer);
         return ResponseEntity.ok(result);
     }
